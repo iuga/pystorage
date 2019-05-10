43cea6c07d3c12a23eec41ae91917fa9db3714bf
@iugax <iugamovil@gmail.com>
2019-05-10 14:59:05 -0300

    0.4.0: Promoting

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="PyStorage",
-    version="0.3.0",
+    version="0.4.1",
     description="Lightweight Python Cache Storage Library",
     author="@iugax",
     packages=find_packages(exclude=["tests", "tools"]),
